1c62c4948837cc2aa439e596c3cb806a26ebe191
Adriano Paz <contato@adrianopaz.com>
2023-10-01 14:06:33 +0900

    Primeiros containers

--- meu_flutter_app/lib/main.dart
+++ meu_flutter_app/lib/main.dart
@@ -15,7 +15,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ,
+      home: Stack(
+        alignment: AlignmentDirectional.center,
+        children: [
+          Container(color: Colors.red,width: 100, height: 100,),
+          Container(color: Colors.blue, width: 50, height: 50,),
+        ],
+      ),
     );
   }
 }
